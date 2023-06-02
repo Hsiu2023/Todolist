b23e66200169c5e966910a01a6b3ae5ac0bc6255
Hsiu <77731262+Hsiu2023@users.noreply.github.com>
2023-06-02 22:14:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -120,5 +120,5 @@ const requestListener=(req,res)=>{
 }
 const server =http.createServer(requestListener);
 
-//要上 Heyroku 要加環境變數 process.ent.PORT ||
+//要上 Heyroku 要加環境變數 process.en.PORT ||
 server.listen(process.env.PORT || 3005);
